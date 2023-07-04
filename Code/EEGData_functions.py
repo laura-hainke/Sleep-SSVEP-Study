52cf8aec397f18e21042070932a0d8fb3e2fd00f
Laura Hainke <hainke.laura@gmail.com>
2023-07-04 17:51:11 +0200

    Update EEG processing pipeline

--- Code/EEGData_functions.py
+++ Code/EEGData_functions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Author: Laura Hainke
-Date: 28.04.2023
+Date: 07.2023
 Functionality: Functions for EEG data processing (Gamma-Sleep Study).
 Assumptions:
 Notes:
@@ -13,19 +13,21 @@ Notes:
 
 # Libraries
 import mne
+import os
+import numpy as np
 
 
-# %% Function: load_edf
+# %% Function: load_raw
 """
-    Load EDF file and perform basic processing.
+    Load raw EEG file in Nihon Kohden format and perform basic processing.
 
     Input
     ----------
     filename : str
-    	Path to the EDF file
+    	Path to the Nihon Kohden file
         
     bad_ch : list
-        Names of bad channels identified visually
+    Names of bad channels identified visually
     
     Output
     -------
@@ -34,57 +36,210 @@ import mne
 
 """
 
-def load_edf(filename,bad_ch=None):
+def load_raw(filename,bad_ch=None):
 
-    # Load raw data
-    raw = mne.io.read_raw_edf(filename, preload=True)
+    # Go to directory containing files (necessary for read_raw_nihon)
+    os.chdir(filename[0:55])
     
-    # Keep only ROI channels
-    raw.pick_channels(['O1-Fz','O2-Fz','Cz-Fz','P3-Fz','P4-Fz','Pz-Fz'])
+    # Load raw data in Nihon Kohden format (Note: takes ~25 min for a full night)
+    raw = mne.io.read_raw_nihon(filename, preload=True)
     
     # Rename channels
-    raw.rename_channels({'O1-Fz':'O1', 'O2-Fz':'O2', 'Cz-Fz':'Oz', 'P3-Fz':'PO3', 'P4-Fz':'PO4', 'Pz-Fz':'POz'})
+    raw.rename_channels({'Cz':'Oz', 'P3':'PO3', 'P4':'PO4', 'Pz':'POz'})
+    
+    # Re-reference all ROI channels to Fz
+    raw.set_eeg_reference(ref_channels=['Fz'])
+    
+    # Keep only ROI channels
+    raw.pick_channels(['O1','O2','Cz','P3','P4','Pz'], ordered=True)
     
     # Mark bad channels, if any
-    if len(bad_ch) == 1:
+    if bad_ch:
+    
+        if len(bad_ch) == 1:
         
-        raw.info['bads'].append(bad_ch)
+            raw.info['bads'].append(bad_ch)
         
-    elif len(bad_ch) > 1:
+        elif len(bad_ch) > 1:
         
-        raw.info['bads'].extend(bad_ch)
+            raw.info['bads'].extend(bad_ch)
         
-    # Print channels included in ROI
-    print('Channels included in analysis: ')
-    raw.info['ch_names']
-        
+    # Move back to main directory
+    os.chdir('C:/Users/Mitarbeiter/Documents/Gamma_Sleep/Code/Processing/')
+    
     return raw
     
     
     
+# %% Function: assign_epochs
+   
+"""
+    Match scored epochs to data points.
+    
+    Input
+    ----------
+    raw : MNE object
+    Output of load_raw()
+    
+    filename : str
+	Path to epoch report file in .txt format
+
+    Output
+    -------
+    data : ndarray
+	Array containing data points and assigned stages
+    
+    raw : MNE object
+    Original raw object with added annotations for epochs
+
+""" 
+
+def assign_epochs(raw, filename):
+    
+    # Load the .txt file containing the epochs
+    with open(filename, 'r') as f:
+        
+        file = f.readlines()
+     
+        
+    ## Extract info on stages
+    
+    # Delete file header
+    file = file[22:]
+    
+    # Delete empty rows
+    file = [row for row in file if row != "\n"]
+    
+    # Delete table header rows
+    file = [row for row in file if "Chin" not in row]
+    
+    # Get nr. of epochs from file and print
+    n_epochs = len(file)
+    print('Nr. of epochs reported:', n_epochs)
+        
+    # Initialize array of epoch stages
+    epoch_stages = np.zeros(n_epochs, dtype=int)
+    
+    # Get relevant data points
+    i = 0
+    while i < len(file):
+        
+        # Split file row into substrings
+        split = file[i].split()
+        
+        # Second-to-last element = stage
+        stage = split[-2]
+          
+        # Convert stage name into int, add to epoch_stages array
+        if stage == 'L': # Lights on (will be ignored in analyses)
+            
+            epoch_stages[i] = 99
+            
+        elif stage == 'W': # Wake
+            
+            epoch_stages[i] = 0
+            
+        elif stage == 'N1': # Sleep stage 1 (will be ignored in analyses)
+            
+            epoch_stages[i] = 1
+            
+        elif stage == 'N2': # Sleep stage 2
+            
+            epoch_stages[i] = 2
+            
+        elif stage == 'N3': # Sleep stage 3
+            
+            epoch_stages[i] = 3
+            
+        elif stage == 'R': # REM sleep
+            
+            epoch_stages[i] = 4
+            
+        elif stage == 'M': # Movement (will be ignored in analyses)
+        
+            epoch_stages[i] = 99
+           
+        # Move to next line
+        i += 1
+
+
+    ## Get recording info
+
+    # Define epoch duration (AASM standard, 30 sec)
+    epoch_len = 30
+    
+    # Get sampling rate
+    sfreq = raw.info['sfreq']
+    
+    # Get recording duration in seconds
+    rec_len = raw._raw_lengths[0]/sfreq
+    rec_len = int(np.floor(rec_len)) # round down, convert to int
+    
+
+    ## For PSD analyses: stage info as annotations
+    
+    # Onset of annotations: every 30 sec, for whole recording
+    onset = list(range(0, rec_len, 30))
+    
+    # Create anotations
+    annotations = mne.Annotations(onset=onset, duration=epoch_len, description=epoch_stages)  
+    
+    # Add to raw object
+    raw.set_annotations(annotations)  
     
     
+    ## For SSVEP analyses: stage info as column in data array
+
+    # Access raw data, convert from Volts to microVolts
+    data = raw.get_data() * 1e6
+    
+    # Transpose to vertical format for better readability
+    data = np.transpose(data)
+    
+    # Sanity check: print nr. of epochs recorded
+    print('Nr. of epochs recorded:', len(data)/sfreq/epoch_len)
+    
+    # Initialize empty column for stage values
+    stages = np.full(len(data),np.nan)
+    
+    # Add stage info to each data point
+    for i in range(len(epoch_stages)):
+        
+        # Data point index of current epoch start
+        epoch_start = int(i * sfreq * epoch_len)
+        
+        # Data point index of current epoch end
+        epoch_end = int(epoch_start + sfreq * epoch_len)
+        
+        # Add stage value to selected data points
+        stages[epoch_start:epoch_end] = epoch_stages[i]
+        
+    # Merge stages array with data array
+    data = np.c_[data, stages]
+    
+    
+    return raw, data
     
     
 # %% SCRATCHPAD
 
-# For PSD metrics:
-PSD_metrics_con = {'PSD_ntrials_W':,
- 'PSD_ntrials_N2':,
- 'PSD_ntrials_N3':,
- 'PSD_ntrials_REM':,
- 'PSD_40Hz_W':,
- 'PSD_40Hz_N2':,
- 'PSD_40Hz_N3':,
- 'PSD_40Hz_REM':,
- 'PSD_SNR_W':,
- 'PSD_SNR_N2':,
- 'PSD_SNR_N3':,
- 'PSD_SNR_REM':}
+# # For PSD metrics:
+# PSD_metrics_con = {'PSD_ntrials_W':,
+#  'PSD_ntrials_N2':,
+#  'PSD_ntrials_N3':,
+#  'PSD_ntrials_REM':,
+#  'PSD_40Hz_W':,
+#  'PSD_40Hz_N2':,
+#  'PSD_40Hz_N3':,
+#  'PSD_40Hz_REM':,
+#  'PSD_SNR_W':,
+#  'PSD_SNR_N2':,
+#  'PSD_SNR_N3':,
+#  'PSD_SNR_REM':}
 
-SSVEP_metrics_con = {'SSVEP_ntrials_W','SSVEP_ntrials_N2','SSVEP_ntrials_N3','SSVEP_ntrials_REM',
-'SSVEP_PTA_W','SSVEP_PTA_N2','SSVEP_PTA_N3','SSVEP_PTA_REM',
-'SSVEP_SNR_W','SSVEP_SNR_N2','SSVEP_SNR_N3','SSVEP_SNR_REM'}
+# SSVEP_metrics_con = {'SSVEP_ntrials_W','SSVEP_ntrials_N2','SSVEP_ntrials_N3','SSVEP_ntrials_REM',
+# 'SSVEP_PTA_W','SSVEP_PTA_N2','SSVEP_PTA_N3','SSVEP_PTA_REM',
+# 'SSVEP_SNR_W','SSVEP_SNR_N2','SSVEP_SNR_N3','SSVEP_SNR_REM'}
     
     
     
--- Code/EEGData_main.py
+++ Code/EEGData_main.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Author: Laura Hainke
-Date: 06.2023
+Date: 07.2023
 Functionality: Script to import and process EEG data (Gamma-Sleep study).
 Assumptions:
-    - 1 EDF file for exp recording, session 01 (~20 min)
-    - 1 EDF file for whole con recording, session 02 (~8 h)
-    - 1 EDF file for sleep exp recording, session 03 (~8 h)
+    - Files for exp recording, session 01 (~20 min)
+    - Files for whole con recording, session 02 (~8 h)
+    - Files for sleep exp recording, session 03 (~8 h)
+    - Nihon Kohden files: .eeg (raw EEG), .pnt (metadata), .log (annotations), .21e (channel & electrode info)
     - 1 .txt file with scored epochs for con recording, session 02
     - 1 .txt file with scored epochs for exp recording, session 03
 Notes:
@@ -17,7 +18,7 @@ Notes:
 # %% Environment Setup
 
 # Import custom functions
-from EEGData_functions import load_edf, assign_epochs
+from EEGData_functions import load_raw, assign_epochs
 
 # Import packages
 import csv
@@ -34,64 +35,55 @@ subject_nr = input("Subject number: ")
 ## Input data
 
 # Path to folders
-path_in = ["D:/Documents/Gamma_Sleep/Data/Raw/" + subject_nr]
+path_in = str("C:/Users/Mitarbeiter/Documents/Gamma_Sleep/Data/Raw/" + subject_nr)
 
-# Path to session 01 EEG data file
-path_ses01_EEG = [path_in + "/Session01/" + subject_nr + "_session01_raw-EEG.edf"]
+# Path to session 01 EEG data files
+path_ses01_EEG = str(path_in + "/Session01/" + subject_nr + "_session01_raw-EEG.eeg")
 
-# Path to session 02 EEG data file
-path_ses02_EEG = [path_in + "/Session02/" + subject_nr + "_session02_raw-EEG.edf"]
+# Path to session 02 EEG data files
+path_ses02_EEG = str(path_in + "/Session02/" + subject_nr + "_session02_raw-EEG.eeg")
 
 # Path to session 02 epoch report file
-path_ses02_epochs = [path_in + "/Session02/" + subject_nr + "_session02_epoch-report.edf"]
+path_ses02_epochs = str(path_in + "/Session02/" + subject_nr + "_session02_epoch-report.txt")
 
-# Path to session 03 EEG data file
-path_ses03_EEG = [path_in + "/Session03/" + subject_nr + "_session03_raw-EEG.edf"]
+# Path to session 03 EEG data files
+path_ses03_EEG = str(path_in + "/Session03/" + subject_nr + "_session03_raw-EEG.eeg")
 
 # Path to session 03 epoch report file
-path_ses03_epochs = [path_in + "/Session03/" + subject_nr + "_session03_epoch-report.edf"]
+path_ses03_epochs = str(path_in + "/Session03/" + subject_nr + "_session03_epoch-report.txt")
 
 
 ## Output data
 # NOTE: sessions 01 and 03 are merged into an 'experimental' condition; session 02 = 'control'
 
 # Path to folders
-path_out = ["D:/Documents/Gamma_Sleep/Data/Derivatives/" + subject_nr]
-
-# Path to processed EEG data file, control condition
-path_con_EEG = [path_out + "/Control/" + subject_nr + "_control_processed-EEG.edf"]
+path_out = str("C:/Users/Mitarbeiter/Documents/Gamma_Sleep/Data/Derivatives/" + subject_nr)
 
 # Path to output EEG metrics data file, control condition
-path_con_metrics_PSD = [path_out + "/Control/" + subject_nr + "_control_PSD-output-metrics.csv"]
-path_con_metrics_SSVEP = [path_out + "/Control/" + subject_nr + "_control_SSVEP-output-metrics.csv"]
+path_con_metrics_PSD = str(path_out + "/Control/" + subject_nr + "_control_PSD-output-metrics.csv")
+path_con_metrics_SSVEP = str(path_out + "/Control/" + subject_nr + "_control_SSVEP-output-metrics.csv")
 
 # Path to output EEG curves data files, control condition
-path_con_spectra_PSD = [path_out + "/Control/" + subject_nr + "_control_PSD-output-spectra.csv"]
-path_con_curves_SSVEP = [path_out + "/Control/" + subject_nr + "_control_SSVEP-output-curves.csv"]
-
-# Path to processed EEG data file, experimental condition
-path_exp_EEG = [path_out + "/Experimental/" + subject_nr + "_experimental_processed-EEG.edf"]
+path_con_spectra_PSD = str(path_out + "/Control/" + subject_nr + "_control_PSD-output-spectra.csv")
+path_con_curves_SSVEP = str(path_out + "/Control/" + subject_nr + "_control_SSVEP-output-curves.csv")
 
 # Path to output EEG metrics data file, experimental condition
-path_exp_metrics_PSD = [path_out + "/Experimental/" + subject_nr + "_experimental_PSD-output-metrics.csv"]
-path_exp_metrics_SSVEP = [path_out + "/Experimental/" + subject_nr + "_experimental_SSVEP-output-metrics.csv"]
+path_exp_metrics_PSD = str(path_out + "/Experimental/" + subject_nr + "_experimental_PSD-output-metrics.csv")
+path_exp_metrics_SSVEP = str(path_out + "/Experimental/" + subject_nr + "_experimental_SSVEP-output-metrics.csv")
 
 # Path to output EEG curves data files, experimental condition
-path_exp_spectra_PSD = [path_out + "/Experimental/" + subject_nr + "_experimental_PSD-output-spectra.csv"]
-path_exp_curves_SSVEP = [path_out + "/Experimental/" + subject_nr + "_experimental_SSVEP-output-curves.csv"]
+path_exp_spectra_PSD = str(path_out + "/Experimental/" + subject_nr + "_experimental_PSD-output-spectra.csv")
+path_exp_curves_SSVEP = str(path_out + "/Experimental/" + subject_nr + "_experimental_SSVEP-output-curves.csv")
 
 
 
 # %% Load & process control data
 
 # Load data, select ROI channels
-raw_s02 = load_edf(path_ses02_EEG)
+raw_s02 = load_raw(path_ses02_EEG)
 
 # Assign scored epochs to data
 data_epochs_s02 = assign_epochs(raw_s02, path_ses02_epochs)
-
-# Reject bad epochs
-raw_clean_con = []
 
 # Compute average PSD per condition
 PSD_spectra_con, PSD_metrics_con = [] 
@@ -104,14 +96,11 @@ SSVEP_curves_con, SSVEP_metrics_con = []
 # %% Load & process experimental data
 
 # Load data, select ROI channels
-raw_s01 = load_edf(path_ses01_EEG) # Session 01: experimental, wake (20 min)
-raw_s03 = load_edf(path_ses03_EEG) # Session 03: experimental, sleep (8 h)
+raw_s01 = load_raw(path_ses01_EEG) # Session 01: experimental, wake (20 min)
+raw_s03 = load_raw(path_ses03_EEG) # Session 03: experimental, sleep (8 h)
 
 # Assign scored epochs to data
 data_epochs_s03 = assign_epochs(raw_s03, path_ses03_epochs)
-
-# Reject bad epochs
-raw_clean_exp = []
 
 # Compute average PSD per expdition
 PSD_spectra_exp, PSD_metrics_exp = [] 

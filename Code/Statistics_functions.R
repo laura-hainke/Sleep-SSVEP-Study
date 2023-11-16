e2d9cd5f169f798df3ccbe72cd4decaaae6708cc
Laura Hainke <hainke.laura@gmail.com>
2023-11-16 17:38:36 +0100

    Refactor data import

--- Code/Statistics_functions.R
+++ Code/Statistics_functions.R
@@ -11,6 +11,143 @@ library("ez")
 
 
 
+
+# Function: initialize_dataframe -------------------------------------------------------------------------------------------
+# Initialize a dataframe for a given set of variables.
+
+## INPUT
+
+# list_IDs : list of chr
+#   List of subjects IDs with fully collected datasets
+
+# df_type : str
+#   Type of dataframe to be created
+
+## OUTPUT
+
+# custom_dataframe : dataframe
+#   Empty dataframe with columns matching the specified data types
+
+initialize_dataframe <- function(list_IDs, df_type = c("demographic","sleep_quality","PSD_metrics","SSVEP_metrics")) {
+  
+  # Get number of participants
+  n_IDs = length(list_IDs)
+  
+  # Create a custom dataframe depending on the variable type
+  if (df_type ==  "demographic") {
+    custom_dataframe = data.frame(ID = as.factor(list_IDs), # Participant numbers
+                                 age = rep(NA, n_IDs),
+                                 sex = rep(NA, n_IDs),
+                                 gender_match = rep(NA, n_IDs),
+                                 handedness = rep(NA, n_IDs),
+                                 education = rep(NA, n_IDs),
+                                 uMCTQ_score = rep(NA, n_IDs),
+                                 PSQI_score = rep(NA, n_IDs))
+  } 
+  else if (df_type == "sleep_quality") {
+    custom_dataframe = data.frame(ID = as.factor(list_IDs), 
+                                  # PSG metrics, condition CON 
+                                  SOL_con = rep(NA, n_IDs), # Sleep Onset Latency
+                                  TST_con = rep(NA, n_IDs), # Total Sleep Time
+                                  WASO_con = rep(NA, n_IDs), # Wake After Sleep Onset 
+                                  # Time per stage as % of TST, condition CON
+                                  perN1_con = rep(NA, n_IDs), # N1
+                                  perN2_con = rep(NA, n_IDs), # N2
+                                  perN3_con = rep(NA, n_IDs), # N3
+                                  perREM_con = rep(NA, n_IDs), # REM
+                                  # Subjective sleep quality, condition CON
+                                  GSQS_con = rep(NA, n_IDs), # Groeningen Sleep Quality Scale sum score
+                                  # PSG metrics, condition EXP 
+                                  SOL_exp = rep(NA, n_IDs), 
+                                  TST_exp = rep(NA, n_IDs),
+                                  WASO_exp = rep(NA, n_IDs),
+                                  # Time per stage as % of TST, condition EXP
+                                  perN1_exp = rep(NA, n_IDs),
+                                  perN2_exp = rep(NA, n_IDs),
+                                  perN3_exp = rep(NA, n_IDs),
+                                  perREM_exp = rep(NA, n_IDs),
+                                  # Subjective sleep quality, condition EXP
+                                  GSQS_exp = rep(NA, n_IDs))
+  } 
+  else if (df_type == "PSD_metrics") {
+    custom_dataframe = data.frame(ID = as.factor(list_IDs), 
+                                  # Stage W, condition CON
+                                  W_nepochs_con = rep(NA, n_IDs), # Nr. of 30 s epochs included in analysis
+                                  W_PSD40_con = rep(NA, n_IDs), # PSD value at 40 Hz
+                                  W_SNR40_con = rep(NA, n_IDs), # SNR value at 40 Hz
+                                  # Stage N2, condition CON
+                                  N2_nepochs_con = rep(NA, n_IDs),
+                                  N2_PSD40_con = rep(NA, n_IDs),
+                                  N2_SNR40_con = rep(NA, n_IDs),
+                                  # Stage N3, condition CON
+                                  N3_nepochs_con = rep(NA, n_IDs),
+                                  N3_PSD40_con = rep(NA, n_IDs),
+                                  N3_SNR40_con = rep(NA, n_IDs),
+                                  # Stage REM, condition CON
+                                  REM_nepochs_con = rep(NA, n_IDs),
+                                  REM_PSD40_con = rep(NA, n_IDs),
+                                  REM_SNR40_con = rep(NA, n_IDs),
+                                  # Stage W, condition EXP
+                                  W_nepochs_exp = rep(NA, n_IDs),
+                                  W_PSD40_exp = rep(NA, n_IDs),
+                                  W_SNR40_exp = rep(NA, n_IDs),
+                                  # Stage N2, condition EXP
+                                  N2_nepochs_exp = rep(NA, n_IDs),
+                                  N2_PSD40_exp = rep(NA, n_IDs),
+                                  N2_SNR40_exp = rep(NA, n_IDs),
+                                  # Stage N3, condition EXP
+                                  N3_nepochs_exp = rep(NA, n_IDs),
+                                  N3_PSD40_exp = rep(NA, n_IDs),
+                                  N3_SNR40_exp = rep(NA, n_IDs),
+                                  # Stage REM, condition EXP
+                                  REM_nepochs_exp = rep(NA, n_IDs),
+                                  REM_PSD40_exp = rep(NA, n_IDs),
+                                  REM_SNR40_exp = rep(NA, n_IDs))
+  } 
+  else if (df_type == "SSVEP_metrics") {
+    custom_dataframe = data.frame(ID = as.factor(list_IDs), 
+                                 # Stage W, condition CON
+                                 W_nsegments_con = rep(NA, n_IDs), # Nr. of 25 ms segments included in analysis
+                                 W_PTA_con = rep(NA, n_IDs), # Peak-to-trough amplitude of SSVEP
+                                 W_SNR_con = rep(NA, n_IDs), # SNR value of SSVEP
+                                 # Stage N2, condition CON
+                                 N2_nsegments_con = rep(NA, n_IDs),
+                                 N2_PTA_con = rep(NA, n_IDs),
+                                 N2_SNR_con = rep(NA, n_IDs),
+                                 # Stage N3, condition CON
+                                 N3_nsegments_con = rep(NA, n_IDs),
+                                 N3_PTA_con = rep(NA, n_IDs),
+                                 N3_SNR_con = rep(NA, n_IDs),
+                                 # Stage REM, condition CON
+                                 REM_nsegments_con = rep(NA, n_IDs),
+                                 REM_PTA_con = rep(NA, n_IDs),
+                                 REM_SNR_con = rep(NA, n_IDs),
+                                 # Stage W, condition EXP
+                                 W_nsegments_exp = rep(NA, n_IDs),
+                                 W_PTA_exp = rep(NA, n_IDs),
+                                 W_SNR_exp = rep(NA, n_IDs),
+                                 # Stage N2, condition EXP
+                                 N2_nsegments_exp = rep(NA, n_IDs),
+                                 N2_PTA_exp = rep(NA, n_IDs),
+                                 N2_SNR_exp = rep(NA, n_IDs),
+                                 # Stage N3, condition EXP
+                                 N3_nsegments_exp = rep(NA, n_IDs),
+                                 N3_PTA_exp = rep(NA, n_IDs),
+                                 N3_SNR_exp = rep(NA, n_IDs),
+                                 # Stage REM, condition EXP
+                                 REM_nsegments_exp = rep(NA, n_IDs),
+                                 REM_PTA_exp = rep(NA, n_IDs),
+                                 REM_SNR_exp = rep(NA, n_IDs))
+  }
+  else {
+    warning("ERROR: Most likely, dataframe type not accepted")
+  }
+  
+  return(custom_dataframe)
+}
+
+
+
 # Function: paired t-test ------------------------------------------------------------------------------------------------------------
 # Run a paired t-test on 2 variables.
 
--- Code/Statistics_main.R
+++ Code/Statistics_main.R
@@ -1,6 +1,6 @@
 # Author: LH
 # Date: 2023-09-21
-# Functionality: Load derivative participant data, run statistical analysis
+# Functionality: Load derivative participant data, generate plots, run statistical analysis
 # Notes:
 
 
@@ -10,10 +10,9 @@
 # Working directory
 setwd("C:\\Users\\Mitarbeiter\\Documents\\Gamma_Sleep\\Code\\Statistics")
 
-# Source functions, files in current directory
+# Function files in current directory
 source("Statistics_functions.r")
 source("Plots_functions.r")
-
 
 # Libraries
 library("pastecs")
@@ -24,219 +23,62 @@ path_derivatives = "C:\\Users\\Mitarbeiter\\Documents\\Gamma_Sleep\\Data\\Deriva
 # List of folder names in directory, corresponding to participant numbers
 list_IDs = list.dirs(path = path_derivatives, full.names = FALSE, recursive = FALSE)
 
-# Eliminate template folder & rejected datasets
-# 
-# For debugging: only complete datasets
-list_IDs = list_IDs[c(2,3,5,6,7,10,12,13,15,19)]
-
-# Get number of participants
-n_IDs = length(list_IDs)
+# Remove rejected datasets (& for debugging, datasets not yet collected)
+list_IDs = list_IDs[-c(3,15,19:22,25:32)]
 
 
 
-# Initialize dataframes ---------------------------------------------------
+# Initialize dataframes for derivative data ---------------------------------------------------
 
-# Initialize dataframe with demographic data from all participants
-data_demo = data.frame(ID = list_IDs, # Participant numbers
-                      age = rep(NA, n_IDs),
-                      sex = rep(NA, n_IDs),
-                      gender_match = rep(NA, n_IDs),
-                      handedness = rep(NA, n_IDs),
-                      education = rep(NA, n_IDs),
-                      uMCTQ_score = rep(NA, n_IDs),
-                      PSQI_score = rep(NA, n_IDs))
+# To be used in statistical analysis
+data_demo = initialize_dataframe(list_IDs, "demographic")
 
-# Initialize dataframe with sleep quality data from all participants
-data_sleep = data.frame(ID = list_IDs, 
-                        # PSG metrics, condition CON 
-                        SOL_con = rep(NA, n_IDs), # Sleep Onset Latency
-                        TST_con = rep(NA, n_IDs), # Total Sleep Time
-                        WASO_con = rep(NA, n_IDs), # Wake After Sleep Onset 
-                        # Time per stage as % of TST, condition CON
-                        perN1_con = rep(NA, n_IDs), # N1
-                        perN2_con = rep(NA, n_IDs), # N2
-                        perN3_con = rep(NA, n_IDs), # N3
-                        perREM_con = rep(NA, n_IDs), # REM
-                        # Subjective sleep quality, condition CON
-                        GSQS_con = rep(NA, n_IDs), # Groeningen Sleep Quality Scale sum score
-                        # PSG metrics, condition EXP 
-                        SOL_exp = rep(NA, n_IDs), 
-                        TST_exp = rep(NA, n_IDs),
-                        WASO_exp = rep(NA, n_IDs),
-                        # Time per stage as % of TST, condition EXP
-                        perN1_exp = rep(NA, n_IDs),
-                        perN2_exp = rep(NA, n_IDs),
-                        perN3_exp = rep(NA, n_IDs),
-                        perREM_exp = rep(NA, n_IDs),
-                        # Subjective sleep quality, condition EXP
-                        GSQS_exp = rep(NA, n_IDs))
+data_sleep = initialize_dataframe(list_IDs, "sleep_quality")
 
-# Initialize dataframe with PSD data from all participants
-data_PSD = data.frame(ID = list_IDs, 
-                      # Stage W, condition CON
-                      W_nepochs_con = rep(NA, n_IDs), # Nr. of 30 s epochs included in analysis
-                      W_PSD40_con = rep(NA, n_IDs), # PSD value at 40 Hz
-                      W_SNR40_con = rep(NA, n_IDs), # SNR value at 40 Hz
-                      # Stage N2, condition CON
-                      N2_nepochs_con = rep(NA, n_IDs),
-                      N2_PSD40_con = rep(NA, n_IDs),
-                      N2_SNR40_con = rep(NA, n_IDs),
-                      # Stage N3, condition CON
-                      N3_nepochs_con = rep(NA, n_IDs),
-                      N3_PSD40_con = rep(NA, n_IDs),
-                      N3_SNR40_con = rep(NA, n_IDs),
-                      # Stage REM, condition CON
-                      REM_nepochs_con = rep(NA, n_IDs),
-                      REM_PSD40_con = rep(NA, n_IDs),
-                      REM_SNR40_con = rep(NA, n_IDs),
-                      # Stage W, condition EXP
-                      W_nepochs_exp = rep(NA, n_IDs),
-                      W_PSD40_exp = rep(NA, n_IDs),
-                      W_SNR40_exp = rep(NA, n_IDs),
-                      # Stage N2, condition EXP
-                      N2_nepochs_exp = rep(NA, n_IDs),
-                      N2_PSD40_exp = rep(NA, n_IDs),
-                      N2_SNR40_exp = rep(NA, n_IDs),
-                      # Stage N3, condition EXP
-                      N3_nepochs_exp = rep(NA, n_IDs),
-                      N3_PSD40_exp = rep(NA, n_IDs),
-                      N3_SNR40_exp = rep(NA, n_IDs),
-                      # Stage REM, condition EXP
-                      REM_nepochs_exp = rep(NA, n_IDs),
-                      REM_PSD40_exp = rep(NA, n_IDs),
-                      REM_SNR40_exp = rep(NA, n_IDs))
+data_PSD = initialize_dataframe(list_IDs, "PSD_metrics")
                         
-# Initialize dataframe with SSVEP data from all participants
-data_SSVEP = data.frame(ID = list_IDs, 
-                      # Stage W, condition CON
-                      W_nsegments_con = rep(NA, n_IDs), # Nr. of 25 ms segments included in analysis
-                      W_PTA_con = rep(NA, n_IDs), # Peak-to-trough amplitude of SSVEP
-                      W_SNR_con = rep(NA, n_IDs), # SNR value of SSVEP
-                      # Stage N2, condition CON
-                      N2_nsegments_con = rep(NA, n_IDs),
-                      N2_PTA_con = rep(NA, n_IDs),
-                      N2_SNR_con = rep(NA, n_IDs),
-                      # Stage N3, condition CON
-                      N3_nsegments_con = rep(NA, n_IDs),
-                      N3_PTA_con = rep(NA, n_IDs),
-                      N3_SNR_con = rep(NA, n_IDs),
-                      # Stage REM, condition CON
-                      REM_nsegments_con = rep(NA, n_IDs),
-                      REM_PTA_con = rep(NA, n_IDs),
-                      REM_SNR_con = rep(NA, n_IDs),
-                      # Stage W, condition EXP
-                      W_nsegments_exp = rep(NA, n_IDs),
-                      W_PTA_exp = rep(NA, n_IDs),
-                      W_SNR_exp = rep(NA, n_IDs),
-                      # Stage N2, condition EXP
-                      N2_nsegments_exp = rep(NA, n_IDs),
-                      N2_PTA_exp = rep(NA, n_IDs),
-                      N2_SNR_exp = rep(NA, n_IDs),
-                      # Stage N3, condition EXP
-                      N3_nsegments_exp = rep(NA, n_IDs),
-                      N3_PTA_exp = rep(NA, n_IDs),
-                      N3_SNR_exp = rep(NA, n_IDs),
-                      # Stage REM, condition EXP
-                      REM_nsegments_exp = rep(NA, n_IDs),
-                      REM_PTA_exp = rep(NA, n_IDs),
-                      REM_SNR_exp = rep(NA, n_IDs))
-                        
+data_SSVEP = initialize_dataframe(list_IDs, "SSVEP_metrics")
 
 
-# Load all derivative participant data ------------------------------------------------------------------------------------------------------------
 
+# Load all derivative participant data into dataframes ------------------------------------------------------------------------------------------------------------
+
+# Subfolders & substrings of the filenames to be imported
+filename_substrings = data.frame(subfolder=c("\\REDCap\\","\\Control\\","\\Experimental\\","\\Control\\","\\Experimental\\","\\Control\\","\\Experimental\\"), file_name=c("_personal-data.csv","_control_sleep-data.csv","_experimental_sleep-data.csv","_control_PSD-output-metrics.csv","_experimental_PSD-output-metrics.csv","_control_SSVEP-output-metrics.csv","_experimental_SSVEP-output-metrics.csv"))
+
+# Loop over all subjects
 for (i in list_IDs) { 
   
-  ## Load demographic data
+  # Loop over all CSV files to be imported
+  for (j in 1:nrow(filename_substrings)) {
+    
+    # Get filename
+    filename = paste(path_derivatives, i, filename_substrings$subfolder[j], i, filename_substrings$file_name[j], sep = "")
+    
+    # Read CSV file
+    data_csv = read.csv(filename, header=FALSE)
+    
+    # Write data to corresponding dataframe
+    if (filename_substrings$file_name[j] == "_personal-data.csv") { 
+      data_demo[data_demo$ID == i,2:6] = data_csv[2,1:5] # Segmenting import to skip variable time_lab_arrival, not relevant for analyses
+      data_demo[data_demo$ID == i,7:8] = data_csv[2,7:8]
+    } else if (filename_substrings$file_name[j] == "_control_sleep-data.csv") {
+      data_sleep[data_sleep$ID == i,2:9] = data_csv[,2]
+    } else if (filename_substrings$file_name[j] == "_experimental_sleep-data.csv") {
+      data_sleep[data_sleep$ID == i,10:17] = data_csv[,2]
+    } else if (filename_substrings$file_name[j] == "_control_PSD-output-metrics.csv") {
+      data_PSD[data_PSD$ID == i,2:13] = data_csv[,2]
+    } else if (filename_substrings$file_name[j] == "_experimental_PSD-output-metrics.csv") {
+      data_PSD[data_PSD$ID == i,14:25] = data_csv[,2]
+    } else if (filename_substrings$file_name[j] == "_control_SSVEP-output-metrics.csv") {
+      data_SSVEP[data_SSVEP$ID == i,2:13] = data_csv[,2]
+    } else if (filename_substrings$file_name[j] == "_experimental_SSVEP-output-metrics.csv") {
+      data_SSVEP[data_SSVEP$ID == i,14:25] = data_csv[,2]
+    }
+    
+  }
   
-  # Get filename
-  file_demo = paste(path_derivatives, i, "\\REDCap\\", i, "_personal-data.csv", sep = "")
-  
-  # Read CSV file
-  data_demo_i = read.csv(file_demo)
-  
-  # Write data to main dataframe
-  # Note: Segmenting import to skip variable time_lab_arrival, which was relevant for planning only
-  data_demo[data_demo$ID == i,2:6] = data_demo_i[1:5] 
-  data_demo[data_demo$ID == i,7:8] = data_demo_i[7:8]
-  
-  
-  ## Load sleep data, CON
-  
-  # Get filename
-  file_sleep_con = paste(path_derivatives, i, "\\Control\\", i, "_control_sleep-data.csv", sep = "")
-  
-  # Read CSV file
-  data_sleep_con_i = read.csv(file_sleep_con, header=FALSE)
-  
-  # Write data to main dataframe
-  data_sleep[data_sleep$ID == i,2:9] = data_sleep_con_i[,2]
-  
-  
-  ## Load sleep data, EXP
-  
-  # Get filename
-  file_sleep_exp = paste(path_derivatives, i, "\\Experimental\\", i, "_experimental_sleep-data.csv", sep = "")
-  
-  # Read CSV file
-  data_sleep_exp_i = read.csv(file_sleep_exp, header=FALSE)
-  
-  # Write data to main dataframe
-  data_sleep[data_sleep$ID == i,10:17] = data_sleep_exp_i[,2]
-  
-  
-  ## Load PSD data, CON
-  
-  # Get filename
-  file_PSD_con = paste(path_derivatives, i, "\\Control\\", i, "_control_PSD-output-metrics.csv", sep = "")
-  
-  # Read CSV file
-  data_PSD_con_i = read.csv(file_PSD_con, header=FALSE)
-  
-  # Write data to main dataframe
-  data_PSD[data_PSD$ID == i,2:13] = data_PSD_con_i[,2]
-  
-  
-  ## Load PSD data,EXP
-  
-  # Get filename
-  file_PSD_exp = paste(path_derivatives, i, "\\Experimental\\", i, "_experimental_PSD-output-metrics.csv", sep = "")
-  
-  # Read CSV file
-  data_PSD_exp_i = read.csv(file_PSD_exp, header=FALSE)
-  
-  # Write data to main dataframe
-  data_PSD[data_PSD$ID == i,14:25] = data_PSD_exp_i[,2]
-  
-  
-  ## Load SSVEP data, CON
-  
-  # Get filename
-  file_SSVEP_con = paste(path_derivatives, i, "\\Control\\", i, "_control_SSVEP-output-metrics.csv", sep = "")
-  
-  # Read CSV file
-  data_SSVEP_con_i = read.csv(file_SSVEP_con, header=FALSE)
-  
-  # Write data to main dataframe
-  data_SSVEP[data_SSVEP$ID == i,2:13] = data_SSVEP_con_i[,2]
-  
-  
-  ## Load SSVEP data, EXP
-  
-  # Get filename
-  file_SSVEP_exp = paste(path_derivatives, i, "\\Experimental\\", i, "_experimental_SSVEP-output-metrics.csv", sep = "")
-  
-  # Read CSV file
-  data_SSVEP_exp_i = read.csv(file_SSVEP_exp, header=FALSE)
-  
-  # Write data to main dataframe
-  data_SSVEP[data_SSVEP$ID == i,14:25] = data_SSVEP_exp_i[,2]
- 
 }
-
-# Remove variables from environment that were only needed in the for loop
-rm(list = ls(pattern = "file_"))
-rm(list = ls(pattern = "_i"))
 
 
 
@@ -253,7 +95,7 @@ stat.desc(data_demo$uMCTQ_score) # not working yet
 
 
 
-# Analysis: GSQS ----------------------------------------------------------
+# Plots: GSQS ----------------------------------------------------------
 
 # Violin plot
 plot_violin(data=data_sleep[,c("ID","GSQS_con","GSQS_exp")], title_plot="Subjective Sleep Quality", title_y="GSQS Sum Score")
@@ -265,7 +107,7 @@ plot_scatter(data=data_sleep[,c("ID","GSQS_con","GSQS_exp")], title_plot="Subjec
 plot_violin_paired(data=data_sleep[,c("ID","GSQS_con","GSQS_exp")], title_plot="Subjective Sleep Quality", title_y="GSQS Sum Score")
 
 
-# Analysis: TST ----------------------------------------------------------
+# Plots: TST ----------------------------------------------------------
 
 # Violin plot
 plot_violin(data=data_sleep[,c("ID","TST_con","TST_exp")], title_plot="Total Sleep Time", title_y="TST (min)")
@@ -275,7 +117,7 @@ plot_scatter(data=data_sleep[,c("ID","TST_con","TST_exp")], title_plot="Total Sl
 
 
 
-# Analysis: WASO ----------------------------------------------------------
+# Plots: WASO ----------------------------------------------------------
 
 # Violin plot
 plot_violin(data=data_sleep[,c("ID","WASO_con","WASO_exp")], title_plot="Wake After Sleep Onset", title_y="WASO (min)")
@@ -285,7 +127,7 @@ plot_scatter(data=data_sleep[,c("ID","WASO_con","WASO_exp")], title_plot="Wake A
 
 
 
-# Descriptive: SOL, % of time per stage --------------------------------------------------------
+# Plots: SOL, % of time per stage --------------------------------------------------------
 
 # Violin plot of SOL
 plot_violin(data=data_sleep[,c("ID","SOL_con","SOL_exp")], title_plot="Sleep Onset Latency", title_y="SOL (min)")
@@ -298,7 +140,7 @@ plot_bar_stack(data_sleep[,c("ID","perN1_con","perN2_con","perN3_con","perREM_co
 
 
 
-# Analysis: PSD ------------------------------------------------------
+# Plots: PSD metrics ------------------------------------------------------
 
 # Boxplots
 plot_box_anova(data_PSD[,c(1,3,6,9,12,15,18,21,24)], title_plot="40 Hz Power", title_y = "PSD (dB)")
@@ -314,7 +156,7 @@ plot_scatter_SNR(data=data_PSD[,c("ID","W_SNR40_con","N2_SNR40_con","N3_SNR40_co
 
 
 
-# Analysis: SSVEP ------------------------------------------------------
+# Plots: SSVEP metrics ------------------------------------------------------
 
 # Boxplots
 plot_box_anova(data_SSVEP[,c(1,3,6,9,12,15,18,21,24)], title_plot="SSVEP Amplitude", title_y = "Amplitude (uV)")
@@ -327,6 +169,10 @@ plot_scatter_main(data=data_SSVEP[,c("ID","W_PTA_con","N2_PTA_con","N3_PTA_con",
 # Connected scatterplot: SNR
 plot_scatter_SNR(data=data_SSVEP[,c("ID","W_SNR_con","N2_SNR_con","N3_SNR_con","REM_SNR_con","W_SNR_exp","N2_SNR_exp","N3_SNR_exp","REM_SNR_exp")],
                     title_plot="SSVEP Amplitude - SNR", title_y="SNR")
+
+
+
+# Plots: SSVEPs -----------------------------------------------------------
 
 
 
